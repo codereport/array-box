360a0ca55e7dc26e4b0eb5fe199d7f6d77321c4b
Conor Hoekstra <codereport@outlook.com>
2026-07-03 13:23:44 -0400

    :sparkles: update

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
  */
 
 const ArrayBoxConfig = {
-    BACKEND_URL: 'https://stopping-sandwich-embedded-color.trycloudflare.com',
+    BACKEND_URL: 'https://conclusions-maryland-raises-gets.trycloudflare.com',
     
     // Example configurations:
     // BACKEND_URL: 'https://arraybox.your-domain.com',     // Custom domain with Cloudflare Tunnel
